11085544b88e0e41d5d24faf1a66487c906d3ad1
Frédéric Gaurat <fgaurat@eolem.com>
2018-06-05 14:17:40 +0200

    ok

--- TPSocketWeb/src/fr/agedi/tpsocketweb/Main.java
+++ TPSocketWeb/src/fr/agedi/tpsocketweb/Main.java
@@ -1,9 +1,37 @@
 package fr.agedi.tpsocketweb;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final String file_url ="accueil-syndicat-intercommunal-agedi_fr.html"; 
+		
+		
+		try {
+			Socket s = new Socket("public.agedi.fr",80);
+			System.out.println("ok");
+			//OutputStream os = s.getOutputStream();
+			PrintStream out = new PrintStream(s.getOutputStream()); 
+			
+			
+			s.close();
+			
+			
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
