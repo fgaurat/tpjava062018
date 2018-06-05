d13092b92b6f1e44a5483450ee70e98075624a4b
Frédéric Gaurat <fgaurat@eolem.com>
2018-06-05 14:27:39 +0200

    ok

--- TPSocketWeb/src/fr/agedi/tpsocketweb/Main.java
+++ TPSocketWeb/src/fr/agedi/tpsocketweb/Main.java
@@ -1,6 +1,8 @@
 package fr.agedi.tpsocketweb;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
@@ -16,8 +18,18 @@ public class Main {
 			Socket s = new Socket("public.agedi.fr",80);
 			System.out.println("ok");
 			//OutputStream os = s.getOutputStream();
-			PrintStream out = new PrintStream(s.getOutputStream()); 
+			PrintStream out = new PrintStream(s.getOutputStream());
+			out.println("GET "+file_url);
+			//BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			InputStreamReader is = new InputStreamReader(s.getInputStream());
+			BufferedReader in = new BufferedReader(is);
 			
+			String html = in.readLine();
+			
+			while(html !=null) {
+				System.out.println(html);
+				html = in.readLine();
+			}
 			
 			s.close();
 			
